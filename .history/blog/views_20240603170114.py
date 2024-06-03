1fc9df553d736e3b66ab829bda230cfd4e8eb419
joaobatistafrontend <jbbuno007@gmail.com>
2024-06-03 17:10:50 -0300

    subindo

---
+++ .history/blog/views_20240603170114.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
+from django.contrib.auth.decorators import login_required
+from .forms import PostForm, CommentForm
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    comments = post.comments.all()
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments, 'form': form})
+
+@login_required
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
